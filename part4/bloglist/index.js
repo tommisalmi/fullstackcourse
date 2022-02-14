fd3fd9418b92d7b91a5a4f03f9b08bc315981acb
Tommi Salmi <tommisalmi96@gmail.com>
2022-02-14 23:50:22 +0200

    Part 4 exercises 4.1-4.2

---
+++ part4/bloglist/index.js
@@ -0,0 +1,17 @@
+const app = require('./app')
+const http = require('http')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+
+const mongoose = require('mongoose')
+
+const server = http.createServer(app) // why don't we use mongoose connect?...
+
+const mongoUrl = config.MONGODB_URI
+logger.info('test')
+mongoose.connect(mongoUrl)
+
+const PORT = config.PORT
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
